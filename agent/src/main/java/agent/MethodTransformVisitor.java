dd51efbcafb1c94a6fcd0c42ea32ebd51425c295
diyohandesilva <50158365+diyohandesilva@users.noreply.github.com>
2019-12-01 21:00:18 -0600

    Var info/ field info

--- agent/src/main/java/agent/MethodTransformVisitor.java
+++ agent/src/main/java/agent/MethodTransformVisitor.java
@@ -3,19 +3,16 @@ package agent;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
-import java.util.Set;
-import java.util.HashSet;
+import org.objectweb.asm.FieldVisitor;
 
 class MethodTransformVisitor extends MethodVisitor implements Opcodes {
 
 	String mName;
     int line;
-    Set<Integer> visited_vars;
 
     public MethodTransformVisitor(final MethodVisitor mv, String name) {
         super(ASM5, mv);
         this.mName = name;
-        this.visited_vars = new HashSet<Integer>();
     }
 
     // statement coverage collection
@@ -33,10 +30,39 @@ class MethodTransformVisitor extends MethodVisitor implements Opcodes {
         
         super.visitLineNumber(line, start);
     }
+    //Local Variables
+    @Override
+    public void visitLocalVariable(String name, String desc,String signature, Label start, Label end, int index) {
+        System.out.println("local variable:"+name);
+        System.out.println("local description:"+desc);
+        System.out.println("local signature:"+signature);
 
+        mv.visitLocalVariable(name, desc, signature, start, end, index);
+
+    }
+
+
+
+    @Override
+    public void visitVarInsn(int opcode, int var) {
+        System.out.println("var instruction"+Integer.toHexString(opcode));
+        System.out.println("var value"+ var);
+        mv.visitVarInsn(opcode, var);
+    }
+
+
+    @Override
+    public void visitFieldInsn(final int opcode,final String owner,final String name,final String descriptor){
+        System.out.println("field:"+name);
+        System.out.println("field owner:"+owner);
+        System.out.println("field descriptor:"+descriptor);
+        mv.visitFieldInsn(opcode, owner, name, descriptor);
+
+    }
     // visit a label
     @Override
     public void visitLabel(Label label) {
+    	
     	if(line != 0)
         {
             mv.visitLdcInsn(mName);
@@ -46,51 +72,5 @@ class MethodTransformVisitor extends MethodVisitor implements Opcodes {
         }
         
         super.visitLabel(label);
-    }
-
-    // Visits a variable instruction
-    @Override
-    public void visitVarInsn(int opcode, int var) {
-        if(visited_vars.contains(var)) {
-            return;
-        }
-
-        String var_type = "";
-        char char_type = (char)0;
-
-        if(opcode == ILOAD){
-            var_type = "java/lang/Integer";
-            char_type = 'I';
-        }
-        else if(opcode == FLOAD){
-            var_type = "java/lang/Float";
-            char_type = 'F';
-        }
-        else if(opcode == LLOAD){
-            var_type = "java/lang/Long";
-            char_type = 'L';
-        }
-        else if(opcode == DLOAD){
-            var_type = "java/lang/Double";
-            char_type = 'D';
-        }
-        else if(opcode == ALOAD) {
-            var_type = "java/lang/Object";
-        }
-        else {
-            return;
-        }
-
-        mv.visitVarInsn(opcode, var);
-        visited_vars.add(var);
-
-        System.out.println("OPCODE-" + opcode + "\t" + var_type + "-TYPE");
-
-        if((int)char_type != 0) {
-            mv.visitMethodInsn(INVOKEVIRTUAL, var_type, "valueOf", "(" + char_type + ")L" + var_type + ";", false);
-        }
-        mv.visitMethodInsn(INVOKEVIRTUAL, "agent/CollectCoverage", "addVariable", "(Ljava/lang/String;Ljava/lang/Integer)V", false);
-
-        super.visitVarInsn(opcode, var);
     }
 }
